cf65b722d9ca1b4eb0ce16b15ecef46a38da9a05
Himanshu Ranjan <84950556+RanjanH@users.noreply.github.com>
2024-01-09 21:29:25 +0530

    data segregation

--- bot.py
+++ bot.py
@@ -5,6 +5,11 @@ import os
 import asyncio
 import requests
 import json
+import threading
+
+from random import randint,choice
+from pokedex import pokedex
+from pkmn_moves import move_list
 
 log.basicConfig(level=log.INFO,filename="logs.log",filemode='w',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
@@ -12,6 +17,11 @@ dotenv.load_dotenv()
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
+
+def listen():
+    loop2 = asyncio.new_event_loop()
+    asyncio.set_event_loop
+    bot.p()
 
 class PSClient:
     def __init__(self):
@@ -22,7 +32,18 @@ class PSClient:
         self.client = None
         self.isConnected = False
         self.isLoggedIn = False
-        self.battles = None
+        self.battles = []
+        self.pokemon = [{},{},{},{},{},{}]
+        self.active_pkmn = [{},{}]
+        self.battleStart = False
+        self.inBattle = False
+        self.player = 1
+        self.self_switch = False
+        self.turn = 1
+        self.frmat = ''
+
+    def p(self):
+        print("done")
 
     async def sendMSG(self,room,message:list):
         msg = room + '|' + '|'.join(message)
@@ -33,7 +54,107 @@ class PSClient:
         msg = await self.client.recv()
         #print(msg)
         log.info(f"Received message from websocket :> {msg}")
+
+        if '|updatesearch|' in msg:
+            text = msg.replace('|updatesearch|','')
+            text = json.loads(text)
+            if text['games'] != None and self.frmat != '':
+                self.battles.append()
+
+        if '|pm|' in msg:
+            text = (msg.split('|pm|'))[1].split('|')
+            sender = text[0][1:]
+            if '/challenge ' in msg:
+                self.frmat = text[3]
+                if 'random' in self.frmat:
+                    await self.sendMSG('',['/utm null'])
+                    await self.sendMSG('',[f'/accept {sender}'])
+                    self.inBattle = True
+                    return
+        
+        if self.inBattle and not self.battleStart:
+            if f"|p2|{self.uName}" in msg:
+                self.player = 2
+            if "|start" in msg:
+                self.battleStart = True
+
+        if "|request|" in msg:
+            info = msg.split('\n')[1]
+            info = info.replace("|request|",'')
+            info = json.loads(info)
+            if info['active'][0]['moves'][0]['id'] == 'recharge':
+                await self.client.send("/choose move 1")
+            else:
+                await self.pkmn_info(info)
+                if "forceSwitch" in info.keys():
+                    if info['forceSwitch']:
+                        self.self_switch = True
+                        await self.switch()
+                elif "wait" in info.keys():
+                    if info['wait']:
+                        return
+                    
+        if "|switch|" in msg and not self.self_switch:
+            self.opp_pkmn(msg)
+
+        if self.battleStart:
+            if f"|turn|{self.turn}" in msg:
+                await self.pick_move()
+            if f"|win|" in msg:
+                info = msg.split("|win|")
+                self.battleStart = False
+                self.inBattle = False
+                if self.uName in msg:
+                    print("-------\n I WON \n-------")
+                else:
+                    print("--------\n I LOST \n--------")
+                
+        if '|updatechallenges|' in msg:
+            print('*************************************************in')
+            res = (msg.split('|updatechallenges|'))[1]
+            response = json.loads()
+            print('------------------------------\n',response)
+            #return '\n**************\n'+msg
         return msg
+    
+    async def pick_move(self):
+        self.self_switch = False
+        self.turn += 1
+        num = [1,2,3,4]
+        pick = num.choice(num)
+        self.client.send(f'/choose move {pick}')
+        return
+    
+    async def switch(self):
+        num = [1,2,3,4,5,6]
+        while True:
+            pick = choice(num)
+            if self.pokemon[pick-1]['hp'] == '0 fnt':
+                num.remove(pick)
+            else:
+                break
+
+        self.client.send(f'/choose switch {pick}')
+
+    def opp_pkmn(self,msg):
+        if self.player == 1:
+            info = msg.split("|switch|p2")[1]
+            info = info.split(":")[1]
+            info = info.split(",")[0]
+            info = info.strip()
+            info = info.split("|")[0]
+            self.active_pkmn[1]["name"] = info
+            self.active_pkmn[1]["type"] = pokedex[info.lower()]["types"]
+        else:
+            info = msg.split("|switch|p1")[1]
+            info = info.split(":")[1]
+            info = info.split(",")[0]
+            info = info.strip()
+            info = info.split("|")[0]
+            self.active_pkmn[1]["name"] = info
+            self.active_pkmn[1]["type"] = pokedex[info.lower()]["types"]
+        
+        print(self.active_pkmn)    
 
     async def connect(self):
         log.info("Connecting.....")
@@ -71,14 +192,43 @@ class PSClient:
             msg = await self.recvMSG()
             #print(msg)
             log.info("Successfully Logged In!")
-            await self.sendMSG('',['/join lobby'])
+            #await self.sendMSG('',['/join lobby'])
             while True:
-                msg = await self.recvMSG()
-                print('**',msg)
+                await self.recvMSG()
         else:
             log.error(f"Could not log in\nDetails : {response.content}")
 
+    async def pkmn_info(self,msg):
+
+        for j in range(6):
+            self.pokemon[j]['name'] = msg['side']['pokemon'][j]['details'].split(',')[0]
+            self.pokemon[j]['hp'] = msg['side']['pokemon'][j]['condition']
+            self.pokemon[j]['active'] = msg['side']['pokemon'][j]['active']
+            self.pokemon[j]['moves'] = msg['side']['pokemon'][j]['moves']
+            self.pokemon[j]['type'] = pokedex[((self.pokemon[j]['name'].lower()).replace('-','')).replace(" ",'')]['types']
+        
+        for i in self.pokemon:
+            if i['active']:
+                self.active_pkmn[0]['name'] = i['name']
+                self.active_pkmn[0]['hp'] = i['hp']
+                self.active_pkmn[0]['type'] = i['type']
+                self.active_pkmn[0]['moves'] = []
+                for j in range(0,4):
+                    self.active_pkmn[0]['moves'].append({})
+                    self.active_pkmn[0]['moves'][j]['name'] = i['moves'][j]
+                    try:
+                        if "active" in msg.keys():
+                            self.active_pkmn[0]['moves'][j]['pp'] = msg['active'][0]['moves'][j]['pp']
+                            self.active_pkmn[0]['moves'][j]['disabled'] = msg['active'][0]['moves'][j]['disabled']
+                    except:
+                        log.error("ACTIVE KEY NOT FOUND",exc_info = True)
+                    self.active_pkmn[0]['moves'][j]['type'] = move_list[self.active_pkmn[0]['moves'][j]['name']]['type']
+        
+        print('******************\n',self.pokemon,'\n')
+        print('********')
+
 bot = PSClient()
+#listen()
 
 asyncio.get_event_loop().run_until_complete(bot.connect())
 asyncio.get_event_loop().run_forever()
